e4bd6222aa38628cb3ecc126d59ef098d0044369
Abdul Shamim <abdul.shamim@jungleworks.co>
2019-02-07 21:28:34 +0530

    Initial Commit

---
+++ OMDBClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OMDBClient
+//
+//  Created by Abdul Shamim on 07/02/19.
+//  Copyright © 2019 Abdul Shamim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
